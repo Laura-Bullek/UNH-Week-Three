ce98ae738e8c19bbd7a4ac2b949c8d177811e005
LB Vik <laura.bullek@gmail.com>
2021-03-18 18:51:09 -0400

    Completed 14-Stu_Iteration project

--- 14-Stu_Iteration/Unsolved/script.js
+++ 14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,12 @@
 //WRITE YOUR CODE BELOW
+
+// Array of five student names
+let students = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon"];
+
+// Logging the total number of elements in the students array to the console
+console.log (students.length);
+
+// Using a for loop, greet each classmate and log to the console
+for (var i = 0; i < students.length; i++) { 
+    console.log("Great to see you " + students[i] + ".");
+}
