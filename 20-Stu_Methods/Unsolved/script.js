f28835e138ea11b6cf552b4b4529a30672067eae
LB Vik <laura.bullek@gmail.com>
2021-03-18 21:33:50 -0400

    Completed 20-Stu_Methods project

--- 20-Stu_Methods/Unsolved/script.js
+++ 20-Stu_Methods/Unsolved/script.js
@@ -3,3 +3,22 @@ var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
 var star = "polaris";
 
 // WRITE YOUR CODE BELOW
+// "Canis Major" is added as the first element of the array
+// No elements are removed
+constellations.unshift("Canis Major"); 
+
+// "Venus" is removed from the planets array
+// The first argument specifies the location at which to begin adding or removing elements. 
+// The second argument specifies the number of elements to remove.
+planets.splice(5,1);
+
+// Arrays constellations and planets are joined to form a new array named galaxy
+constellations.concat(planets);
+
+// "Polaris" is converted into all capital letters and
+// the new string is stored in a variable
+let starCaps = star.toUpperCase()
+
+console.log(constellations);
+console.log(planets);
+console.log(starCaps);
